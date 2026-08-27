706012ca18b5f1ab519f5e7967719ed6fc825ec1
adib <solutifmultikonsultindo@gmail.com>
2026-08-28 06:10:53 +0700

    update verify

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
         });
         
         // ----- Semua user yang login & sudah verifikasi email -----
-        Route::middleware(['auth', 'verified'])->group(function () {
+        Route::middleware(['auth'])->group(function () {
             Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
             
             Route::get('/catalog', [BookController::class, 'index'])->name('catalog.index');
